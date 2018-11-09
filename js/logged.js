aa8ca05d6b20c753fa4cf922f1fe5b8bec800f24
Chibueze <eziokwubunduchibueze@gmail.com>
2018-11-09 15:17:01 +0100

    Get requests

--- js/logged.js
+++ js/logged.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
+    $.get( "'http://localhost:3000/product/'", function( data ) {
+    $( "#table" )
+        .append( '<tr>'+'<td>' + data.productName + '</td>' + '<td>' + data.category + '</td>'+ '<td>' + data.stock + '</td>'+ '<td>' + data.unitPrice + '</td>'+'<tr>');
+    }, "json" );
+    var uniqId = (function(){
+        var i=0;
+        return function() {
+            return i++;
+        }
+    })();
     function Product( productName, category, stock, unitPrice){
+        this.id = uniqId();
         this.productName = productName;
         this.category = category;
         this.stock = stock;
@@ -15,7 +26,7 @@ $(document).ready(function(){
         }
         return fetch(baseUrl, options)
           .then((response) => response.json)
-    }
+    }    
     $("#addProduct").click(function() {
         var a = $("#nameP").val();
         var b = $("#cat").val()
@@ -24,7 +35,9 @@ $(document).ready(function(){
         let product = new Product(a ,b , c, d)
         baseUrl = "http://localhost:3000/products";
         create(product);
+        //$('#heading').append('<th>'+ 'Product Name'+'</th>'+'<th>' + 'Category' +'</th>'+'<th>'+ 'Stock' +'</th>'+'<th>'+'Unit Price'+'</th>')
+        $('#table').append( '<tr>'+'<td>' + a + '</td>' + '<td>' + b + '</td>'+ '<td>' + c + '</td>'+ '<td>' + d + '</td>'+'<tr>')
     })
     
+    
 })
-
--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+    var uniqId = (function(){
+        var i=0;
+        return function() {
+            return i++;
+        }
+    })();
     function User(cName, regNo, cAddress, email, password){
+        this.id = uniqId()
         this.cName = cName;
         this.regNo = regNo
         this.cAddress = cAddress;
@@ -30,3 +37,30 @@ $(document).ready(function(){
       })
 })
 
+function get() {
+    let url = 'http://localhost:3000/product/';
+    $.get(rl,function () {
+
+    });
+    
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+  
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        renderResponse(xhr.response);
+      }
+    }
+    xhr.open('GET', url);
+    xhr.send();
+  }
+  // Formats response to look presentable on webpage
+const renderResponse = (res) => {
+    let inventory = []
+    for(let i = 0; i < res.length; i++){
+      inventory.push(``)
+    wordList = wordList.join("")
+    responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`
+    return
+  }
+}
