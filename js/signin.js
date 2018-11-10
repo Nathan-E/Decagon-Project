79637bf00bbb83e34cc279460f416f227b7bcc89
Chibueze <eziokwubunduchibueze@gmail.com>
2018-11-10 15:02:15 +0100

    navigation success

--- js/signin.js
+++ js/signin.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
-    $.get( "http://localhost:3000/users", function( data ) {
-        for( i in data){
-            if ($('#email1').val() === $(data[i].users.email)){
-                if ($('#pass1').val() === $(data[i].users.password)) {
-                    $('#signin').click(function() {
-                        location.replace('file:///C:/Users/user/Documents/Decagon_Bootcamp/1stLab/Second-Set-Of-Lab/New%20folder/Project/Decagon-Project/pages/logged.html#')
-                    })
+    $('#login').click(function () {
+        let ema = $('#email1').val();
+        let pas = $('#pass1').val();
+        console.log(pas)
+    $.get( "http://localhost:3000/users?email="+ema, function( data ) {
+            let obj = JSON.stringify(data[0].password);
+            pas = JSON.stringify(pas)
+                if (( pas == obj)){ 
+                location.replace('file:///C:/Users/user/Documents/Decagon_Bootcamp/1stLab/Second-Set-Of-Lab/New%20folder/Project/Decagon-Project/pages/logged.html#')
                 }
-            }
-    }}, "json" )
+                else{
+                    alert("hhey")
+                }
+    }, "json" )
+    })
 })
 
 //window.location.replace("http://www.w3schools.com")
