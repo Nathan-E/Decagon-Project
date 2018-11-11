14d986354529a4facfc8221d10e849ded0831fc0
Chibueze <eziokwubunduchibueze@gmail.com>
2018-11-11 14:35:50 +0100

    log out functional

--- js/logged.js
+++ js/logged.js
@@ -1,43 +1,88 @@
-$(document).ready(function(){
-    $.get( "http://localhost:3000/products", function( data ) {
-        for( i in data){
-            $( "#table" ).append( '<tr>'+'<td>' + data[i].productName + '</td>' + '<td>' + data[i].category + '</td>'+ '<td>' + data[i].stock + '</td>'+ '<td>' + data[i].unitPrice + '</td>'+'<tr>');
-    }}, "json" );
+const userDetails = localStorage.getItem("userDetails");
+if (!userDetails) {
+  alert("Not logged in");
+  window.location.href = "../index.html";
+}
+$(document).ready(function() {
+  const userId = JSON.parse(userDetails).id;
+  $.get(
+    `http://localhost:3000/products?userId=${userId}`,
+    function(data) {
+      for (i in data) {
+        $("#table").append(
+          "<tr>" +
+            "<td>" +
+            data[i].productName +
+            "</td>" +
+            "<td>" +
+            data[i].category +
+            "</td>" +
+            "<td>" +
+            data[i].stock +
+            "</td>" +
+            "<td>" +
+            data[i].unitPrice +
+            "</td>" +
+            "<tr>"
+        );
+      }
+    },
+    "json"
+  );
 
-    var uniqId = (function(){
-        var i=0;
-        return function() {
-            return i++;
-        }
-    })();
-    function Product( productName, category, stock, unitPrice){
-        this.id = uniqId();
-        this.productName = productName;
-        this.category = category;
-        this.stock = stock;
-        this.unitPrice = unitPrice
-    }
-    function create(data) {
-        let options = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        }
-        return fetch(baseUrl, options)
-          .then((response) => response.json)
-    }    
-    $("#addProduct").click(function() {
-        var a = $("#nameP").val();
-        var b = $("#cat").val()
-        var c = $("#stock").val();
-        var d = $("#unitP").val();
-        let product = new Product(a ,b , c, d)
-        baseUrl = "http://localhost:3000/products";
-        create(product);
-        //$('#heading').append('<th>'+ 'Product Name'+'</th>'+'<th>' + 'Category' +'</th>'+'<th>'+ 'Stock' +'</th>'+'<th>'+'Unit Price'+'</th>')
-        $('#table').append( '<tr>'+'<td>' + a + '</td>' + '<td>' + b + '</td>'+ '<td>' + c + '</td>'+ '<td>' + d + '</td>'+'<tr>')
-    })
-    
-})
+  $("#log-out").click(() => {
+    window.location.href = "../index.html";
+    localStorage.clear();
+  });
+  var uniqId = (function() {
+    var i = 0;
+    return function() {
+      return i++;
+    };
+  })();
+  function Product(productName, category, stock, unitPrice, userId) {
+    this.id = uniqId();
+    this.productName = productName;
+    this.category = category;
+    this.stock = stock;
+    this.unitPrice = unitPrice;
+    this.userId = userId;
+  }
+  function create(data) {
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    };
+    return fetch(baseUrl, options).then(response => response.json);
+  }
+  $("#addProduct").click(function() {
+    const userDetails = JSON.parse(localStorage.getItem("userDetails"));
+    var a = $("#nameP").val();
+    var b = $("#cat").val();
+    var c = $("#stock").val();
+    var d = $("#unitP").val();
+    let product = new Product(a, b, c, d, userDetails.id);
+    baseUrl = "http://localhost:3000/products";
+    create(product);
+    //$('#heading').append('<th>'+ 'Product Name'+'</th>'+'<th>' + 'Category' +'</th>'+'<th>'+ 'Stock' +'</th>'+'<th>'+'Unit Price'+'</th>')
+    $("#table").append(
+      "<tr>" +
+        "<td>" +
+        a +
+        "</td>" +
+        "<td>" +
+        b +
+        "</td>" +
+        "<td>" +
+        c +
+        "</td>" +
+        "<td>" +
+        d +
+        "</td>" +
+        "<tr>"
+    );
+  });
+});
--- js/signin.js
+++ js/signin.js
@@ -1,19 +1,22 @@
-$(document).ready(function(){
-    $('#login').click(function () {
-        let ema = $('#email1').val();
-        let pas = $('#pass1').val();
-        console.log(pas)
-    $.get( "http://localhost:3000/users?email="+ema, function( data ) {
-            let obj = JSON.stringify(data[0].password);
-            pas = JSON.stringify(pas)
-                if (( pas == obj)){ 
-                location.replace('file:///C:/Users/user/Documents/Decagon_Bootcamp/1stLab/Second-Set-Of-Lab/New%20folder/Project/Decagon-Project/pages/logged.html#')
-                }
-                else{
-                    alert("hhey")
-                }
-    }, "json" )
-    })
-})
+$(document).ready(function() {
+  $("#login").click(function() {
+    let ema = $("#email1").val();
+    let pas = $("#pass1").val();
+    $.get(
+      "http://localhost:3000/users?email=" + ema,
+      function(data) {
+        let obj = JSON.stringify(data[0].password);
+        pas = JSON.stringify(pas);
+        if (pas == obj) {
+          localStorage.setItem("userDetails", JSON.stringify(data[0]));
+          window.location.href = "./pages/logged.html";
+        } else {
+          alert("hhey");
+        }
+      },
+      "json"
+    );
+  });
+});
 
 //window.location.replace("http://www.w3schools.com")
