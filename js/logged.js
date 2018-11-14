eca6e040fe98e5a3fa6c904a825d395182b41f7f
Chibueze <eziokwubunduchibueze@gmail.com>
2018-11-14 11:57:53 +0100

    Delete working

--- js/logged.js
+++ js/logged.js
@@ -3,6 +3,42 @@ if (!userDetails) {
   alert("Not logged in");
   window.location.href = "../index.html";
 }
+function productDelete(x) {
+
+  let path = `http://localhost:3000/products/${x}`;
+
+  $.ajax({
+    url: path,
+    method: "delete",
+
+    success: function(response) {
+      alert("deleted")
+      ;
+    },
+    error: function() {
+      alert("error");
+    }
+  });
+}
+
+  function productUpdate(x) {
+
+    let path = `http://localhost:3000/products/${x}`;
+  
+    $.ajax({
+      url: path,
+      method: "delete",
+  
+      success: function(response) {
+        alert("deleted")
+        ;
+      },
+      error: function() {
+        alert("error");
+      }
+    });
+}
+
 $(document).ready(function() {
   const userId = JSON.parse(userDetails).id;
   $.get(
@@ -12,11 +48,16 @@ $(document).ready(function() {
         $("#table").append(
           `<tr><td>${data[i].productName}</td><td>${data[i].category}</td><td>${
             data[i].stock
-          }</td><td>${data[i].unitPrice}</td>
+          }</td><td>$${data[i].unitPrice}</td>
           <td>
-          <a href='../logged.html?id=${
+          <button type='button' id="${
             data[i].id
-          }'><button type='button'>DEL</button></a>
+          }" onclick="productDelete(this.id)">Delete</button>
+          </td>
+          <td>
+          <button type='button' id="${
+            data[i].id
+          }" onclick="productUpdate(this.id)">Update</button>
           </td>
           <tr>`
         );
@@ -25,7 +66,25 @@ $(document).ready(function() {
     "json"
   );
 
-  
+  // function productDelete() {
+  //   console.log("hello world");
+  //   // let id = $(this).attr("id");
+
+  //   let path = `http://localhost:3000/products/${x}`;
+
+  //   $.ajax({
+  //     url: path,
+  //     method: "delete",
+
+  //     success: function(response) {
+  //       alert("deleted");
+  //     },
+  //     error: function() {
+  //       alert("error");
+  //     }
+  //   });
+  // }
+
   $("#log-out").click(() => {
     window.location.href = "../index.html";
     localStorage.clear();
@@ -63,18 +122,19 @@ $(document).ready(function() {
     const c = $("#stock").val();
     const d = $("#unitP").val();
     let product = new Product(a, b, c, d, userDetails.id);
+
     baseUrl = "http://localhost:3000/products";
     create(product);
-    const e = product.id;
-    console.log(e)
     $("#table").append(
-      `<tr><td>${a}</td><td>${b}</td><td>${c}</td><td>${d}</td>
+      `<tr><td>${a}</td><td>${b}</td><td>${c}</td><td>$${d}</td>
       <td>
-      <a href='../delete.html?id=${e}'><button type='button'>DEL</button></a>
+      <button type='button' type='button' id='${
+        product.id
+      }' onclick='productDelete'()>DEL</button></a>
       </td>
       <tr>`
     );
   });
-
 });
+
 
